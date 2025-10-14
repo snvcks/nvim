1c85a01d4ba561bff5b9c26604e507edfeae7518
= <=>
2025-10-14 17:34:34 +1100

    added recent project and kotofetch to dashboard, and sparkly to display spells

--- lua/plugins/dashboard.lua
+++ lua/plugins/dashboard.lua
@@ -6,37 +6,71 @@ return {
             -- your dashboard configuration comes here
             -- or leave it empty to use the default settings
             -- refer to the configuration section below
+
             sections = {
+                -- Dummy spacing
+                {
+                    pane = 1,
+                    section = "terminal",
+                    cmd = "kotofetch",
+                    height = 9,
+                    padding = 1,
+                },
+                {
+                    pane = 2,
+                    section = "terminal",
+                    cmd = [[
+                    shuf -n 1 ~/dev/spell.txt | awk '{
+                        for (i=1; i<=60; i++) {
+                          c = substr($0, (i-1)%length($0)+1, 1)
+                          v = index("abcdefghijklmnopqrstuvwxyz", tolower(c))
+                          printf("%d ", (v ? v : 0))
+                        }
+                        print ""
+                      }' | sparkly
+                    ]],
+                    height = 1,
+                    padding = 2,
+                },
                 -- the chafa img
                 {
                     pane = 1,
                     section = "terminal",
-                    cmd = "chafa ~/.config/images/snow1.png --format symbols --symbols vhalf --size 60x17 --stretch --probe off; sleep .1",
+                    cmd =
+                    "chafa ~/.config/images/snow1.png --format symbols --symbols vhalf --size 60x17 --stretch --probe off; sleep .1",
                     height = 16,
                     padding = 0,
                 },
-                -- the keys 
-                { section = "keys", gap = 1, padding = 1, pane = 2 },
-                -- recent_files
-                { pane = 3, icon = " ", title = "Recent Files", section = "recent_files", indent = 2, padding = 1 },
-                --project 
-                { pane = 3, icon = " ", title = "Projects", section = "projects", indent = 2, padding = 1 },
-                -- git 
+                -- the keys
                 {
-                    pane = 3,
-                    icon = " ",
-                    title = "Git Status",
-                    section = "terminal",
-                    enabled = function()
-                        return Snacks.git.get_root() ~= nil
-                    end,
-                    cmd = "git status --short --branch --renames",
-                    height = 5,
-                    padding = 1,
-                    ttl = 5 * 60,
-                    indent = 3,
+                    section = "keys",
+                    gap = 0,
+                    padding = 2,
+                    pane = 2,
+                    title = "Keys",
+                    icon = "󰌌"
                 },
-                { section = "startup" ,pane = 2},
+                -- recent_files
+                {
+                    pane = 2,
+                    icon = " ",
+                    title = "Recent Files",
+                    section = "recent_files"
+                    ,
+                    indent = 2,
+                    padding = 2,
+                    gap = 0
+                },
+                --project
+                {
+                    pane = 2,
+                    icon = " ",
+                    title = "Projects",
+                    section = "projects",
+                    indent = 2,
+                    padding = 2,
+                    gap = 0
+                },
             },
         }
     }
